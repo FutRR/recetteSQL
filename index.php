cad6ad37a0c175d54cb3f5aab6b7feee5c63ef80
FutRR <futterermaxime@gmail.com>
2024-02-27 16:14:08 +0100

    added list to switch statement and images to recipe details

--- index.php
+++ index.php
@@ -1,38 +1,26 @@
-<?php
-session_start();
-require_once(__DIR__ . '/config/mysql.php');
-require_once(__DIR__ . '/databaseconnect.php');
-require_once(__DIR__ . '/variables.php');
-require_once(__DIR__ . '/functions.php');
-ob_start();
-?>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Recettes</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+</head>
+
+<body class="bg-light">
+
+    <?php require_once(__DIR__ . "/navbar.php"); ?>
+
+    <div class="container">
+
+        <h1 class="mt-5 pt-5">Maxithon</h1>
+
+        <?php require_once(__DIR__ . "/traitement.php"); ?>
+        <?= $content ?>
 
 
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
 
-<?php foreach (getRecettes($recettes) as $recette): ?>
-    <article>
-        <a class="mt-5 link-dark link-offset-2 link-underline-opacity-0 link-underline-opacity-100-hover"
-            href="traitement.php?action=infosRecettes&id=<?= $recette['id_recette'] ?>">
-            <?= $recette['nomRecette']; ?>
-        </a>
-        |
-        <a class="link-dark link-offset-2 link-underline-opacity-0 link-underline-opacity-100-hover" href="">
-            <?= $recette['nomCategorie'] ?>
-        </a>
-        <p class="">
-            <?= $recette['instructions']; ?>
-        </p>
-    </article>
-<?php endforeach ?>
-
-<script>
-    if (document.title != "Recettes") {
-        document.title = "Recettes";
-    }
-</script>
-
-<?php
-$content = ob_get_clean();
-
-require_once "template.php";
-?>
+</html>
--- traitement.php
+++ traitement.php
@@ -12,6 +12,26 @@ if (isset($_GET["action"])) {
 
     switch ($_GET["action"]) {
         case "listerRecettes":
+            ob_start();
+            foreach (getRecettes($recettes) as $recette) {
+                echo "<article class='m-2'>
+                    <a class='mt-5 link-dark link-offset-2 link-underline-opacity-0 link-underline-opacity-100-hover'
+                        href='traitement.php?action=infosRecettes&id={$recette['id_recette']}'>
+                        {$recette['nomRecette']}
+                    </a>
+                    |
+                    <a class='link-dark link-offset-2 link-underline-opacity-0 link-underline-opacity-100-hover' href=''>
+                        {$recette['nomCategorie']}
+                    </a>
+                </article>
+                <script>
+                    if (document.title != 'Recettes') {
+                        document.title = 'Recettes';
+                    } </script>";
+
+            }
+            $content = ob_get_clean();
+            require_once "index.php";
             break;
 
         case "infosRecettes":
@@ -19,11 +39,10 @@ if (isset($_GET["action"])) {
                 $index = $_GET["id"];
 
                 // Recipe info Request
-                $sql = "SELECT recette.nomRecette, recette.tempsPreparation, recette.instructions, categorie.nomCategorie, ingredient.nomIngredient
+                $sql = "SELECT recette.nomRecette, recette.tempsPreparation, recette.instructions, recette.image,categorie.nomCategorie
                         FROM recette
                         INNER JOIN categorie ON recette.id_categorie = categorie.id_categorie
                         INNER JOIN contenir ON recette.id_recette = contenir.id_recette
-                        INNER JOIN ingredient ON contenir.id_ingredient = ingredient.id_ingredient
                         WHERE recette.id_recette = :id_recette";
 
                 $detailStatement = $mysqlClient->prepare($sql, [PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY]);
@@ -31,26 +50,34 @@ if (isset($_GET["action"])) {
                 $details = $detailStatement->fetch();
 
                 //Ingredients list Request
-                $sql = "SELECT ingredient.nomIngredient, uniteMesure, contenir.quantite
+                $sql = "SELECT ingredient.nomIngredient, ingredient.uniteMesure, contenir.quantite, 
+                            ROUND(SUM(ingredient.prixIngredient*contenir.quantite), 2) AS prixTotal
                         FROM ingredient
                         INNER JOIN contenir ON ingredient.id_ingredient = contenir.id_ingredient
                         INNER JOIN recette ON contenir.id_recette = recette.id_recette
-                        WHERE recette.id_recette = :id_recette";
+                        WHERE recette.id_recette = :id_recette
+                        GROUP BY ingredient.id_ingredient, nomIngredient, uniteMesure, contenir.quantite";
 
                 $listIngredientStatement = $mysqlClient->prepare($sql, [PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY]);
                 $listIngredientStatement->execute(["id_recette" => $index]);
                 $listIngredients = $listIngredientStatement->fetchAll();
 
+                //Starting Output buffering
                 ob_start();
 
                 if ($details) {
                     echo "<h2>Détails de la recette : " . $details['nomRecette'] . "</h2>";
-                    echo "<h3>Ingrédients : </h3><ul>";
-                    foreach ($listIngredients as $ingredient) {
-                        echo "<li>{$ingredient['nomIngredient']} - {$ingredient['quantite']} {$ingredient['uniteMesure']}</li>";
+                    if (isset($ingredient)) {
+                        echo "<h3>Ingrédients : </h3><ul>";
+                        foreach ($listIngredients as $ingredient) {
+                            echo "<li>{$ingredient['nomIngredient']} - {$ingredient['quantite']} {$ingredient['uniteMesure']} | environ {$ingredient['prixTotal']} €</li>";
+                        }
+                        echo "</ul>";
+                    } else {
+                        echo "<h3>Pas d'ingrédients</h3>";
                     }
-                    echo "</ul>";
                     echo "<h4>Instructions : </h4><p>{$details['instructions']}</p>";
+                    echo "<img class='img-fluid' src='{$details['image']}' alt='Image de la recette'/>";
                 } else {
                     echo "Pas de résultats";
                 }
@@ -58,14 +85,34 @@ if (isset($_GET["action"])) {
                 echo "Aucun ID trouvé";
             }
             echo "<script> 
-                    if (document.title != '{$details['nomRecette']}') {
-                    document.title = '{$details['nomRecette']}';
-                    }
-                </script>";
-            $content = ob_get_clean();
+            if (document.title != '{$details['nomRecette']}') {
+            document.title = '{$details['nomRecette']}';
+            } </script>";
 
-            require_once "template.php";
+            //Ending Output buffering and assigning the code to $content variable
+            $content = ob_get_clean();
+            require_once "index.php";
 
             break;
     }
+} else {
+    ob_start();
+    foreach (getRecettes($recettes) as $recette) {
+        echo "<article class='m-2'>
+            <a class='mt-5 link-dark link-offset-2 link-underline-opacity-0 link-underline-opacity-100-hover'
+                href='traitement.php?action=infosRecettes&id={$recette['id_recette']}'>
+                {$recette['nomRecette']}
+            </a>
+            |
+            <a class='link-dark link-offset-2 link-underline-opacity-0 link-underline-opacity-100-hover' href=''>
+                {$recette['nomCategorie']}
+            </a>
+        </article>
+        <script>
+        if (document.title != 'Recettes') {
+            document.title = 'Recettes';
+        } </script>";
+    }
+    $content = ob_get_clean();
+    require_once "index.php";
 }
